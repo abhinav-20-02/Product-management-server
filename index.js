4d749ef8d77f1d4a4eafab12efbd0ea8d875c5de
abhinav@2002 <abhinavabhi1011@gmail.com>
2026-03-02 14:41:18 +0530

    v3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const serverApp = jsonServer.create()
 const routes = jsonServer.router('db.json')
 const middleware = jsonServer.defaults()
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 serverApp.use(middleware)
 serverApp.use(routes)
